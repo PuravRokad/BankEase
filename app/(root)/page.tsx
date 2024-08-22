83427220e97ee3f6d31ad3af04afd8f3dd66fb23
Purav Rokad <rokadpurav06@gmail.com>
2024-08-22 12:36:52 +0530

    Implement Home page UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,27 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = {firstname: 'User1'}
   return (
-    <div>Home</div>
+    <section className="home">
+        <div className="home-content">
+            <header className="home-header">
+                <HeaderBox 
+                type="greeting"
+                title="Welcome"
+                user={loggedIn? loggedIn.firstname : "Guest"}
+                subtext="Access and manage your account and transaction efficiently."
+                />
+            </header>
+            <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1500000}
+            />
+        </div>
+    </section>
   )
 }
 
