f66e787cedbd1567da8442cabc55971b2985b25d
unknown <arhamshah1310@gmail.com>
2024-09-20 11:09:10 +0530

    Logo

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -20,7 +20,7 @@ const Sidebar = ({ user }: SidebarProps) => {
             alt="Horizon logo"
             className="size-[24px] max-xl:size-14"
           />
-          <h1 className="sidebar-logo">Horizon</h1>
+          <h1 className="sidebar-logo">BankEase</h1>
         </Link>
 
         {sidebarLinks.map((item) => {
