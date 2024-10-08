d1227ee2ee85a93dc5ce18f651c9f3a5c9691419
unknown <arhamshah1310@gmail.com>
2024-10-08 12:50:07 +0530

    right-sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -17,7 +17,7 @@ const Sidebar = ({ user }: SidebarProps) => {
             src="/icons/logo.svg"
             width={34}
             height={34}
-            alt="Horizon logo"
+            alt="logo"
             className="size-[24px] max-xl:size-14"
           />
           <h1 className="sidebar-logo">BankEase</h1>
