508509a914fcfed7a85a4078f1a9510f25b8e5ac
Purav Rokad <rokadpurav06@gmail.com>
2024-10-10 11:54:49 +0530

    resolve signin

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -57,8 +57,10 @@ const AuthForm = ({ type }: { type: string }) => {
 
       if(type === 'sign-in'){
         const response = await signIn(
+          {
           email: data.email,
           password: data.password,
+          }
         )
         if(response) router.push('/')
       }
